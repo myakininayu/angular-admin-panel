3a903c8cacc2829820c493e12b31950c25e3f6b9
Myakinina Yulia <myakinina_yu@mail.ru>
2023-08-16 14:56:24 +0300

    Add Redirect to Login Page from start

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: '**', component: NotFoundComponent }
 ];
 
